85bc5882c34e1c4f501737b882270fbb3650bdb1
Jayman Matthews <jaymanmatthews32@gmail.com>
2020-11-11 04:55:13 +0800

    P.O.C 1.25: Added first two BASIC upgrades. Refactored a bit aswell.

--- main.js
+++ main.js
@@ -9,27 +9,31 @@ class Game{
                 coins: 0,
                 cps: 1,
                 time: 0,
-                tab: tabNames[0],
-                base: {
-                    cps: 1
-                }
+                tab: TAB_NAMES[0]
             },
             upgrades: [
                 {
                     cost: 25,
-                    multi: 2,
+                    multi: 1,
                     isBought: false,
                     type: 2,
                     num: 1
                 },
                 {
-                    inc: 3,
                     cost: 40,
+                    multi: 2,
                     isBought: false,
                     type: 2,
                     num: 2
                 }
             ]
+        };
+    }
+
+    static baseValues(){
+        return {
+            cps: new Decimal(1),
+            multi: new Decimal(1)
         };
     }
 
@@ -40,8 +44,8 @@ class Game{
     load(){
         const gameState = JSON.parse(localStorage.getItem('savedGame'));
         this.state = {...this.state, ...gameState};
-        toDecimal(stateNames.main, this.state.main);
-        toDecimal(baseNames, this.state.main.base);
+        toDecimal(STATE_NAMES.main, this.state.main);
+        this.base = Game.baseValues();
         this.upgrades = this.state.upgrades.map(upgradeState => new Upgrade(upgradeState)); // The upgrades are mapped into upgrade objects using their state, filling an upgrades array.
         this.showTab(this.currentTab);
     }
@@ -87,21 +91,47 @@ class Game{
     }
 
     set baseCoinsPerSec(baseCps){
-        this.state.main.base.cps = baseCps;
+        this.base.cps = baseCps;
     }
 
     get baseCoinsPerSec(){
-        return this.state.main.base.cps;
+        return this.base.cps;
     }
 
-    addToValue(value, gain, x, useCase){
+    set baseMultiplier(baseMulti){
+        this.base.multi = baseMulti;
+    }
+
+    get baseMultiplier(){
+        return this.base.multi;
+    }
+
+    set firstUpgrade(upgrade){
+        this.upgrades[0] = upgrade;
+    }
+
+    get firstUpgrade(){
+        return this.upgrades[0];
+    }
+
+    set secondUpgrade(upgrade){
+        this.upgrades[1] = upgrade;
+    }
+
+    get secondUpgrade(){
+        return this.upgrades[1];
+    }
+
+    addToValue(value, gain, useCase){
         switch (useCase) {
             case 1: // Both value and gain converted to decimals.
-                return value.add(gain.div(x));
+                return value.add(gain.mul(MAX_TIME / SECOND));
             case 2: // Value in decimal form, but gain is not in decimal form.
-                return value.add((gain) / x);
+                return value.add((gain) * (MAX_TIME / SECOND));
             case 3: // Both value and gain are not decimals.
-                return value + ((gain) / x);
+                return value + (gain);
+            case 4: // Value is a decimal and needs to be log 10'd
+                return value.add(gain).log10();
         }
     }
 
@@ -125,8 +155,8 @@ class Game{
 
     showTab(tabName){
         let tabs = [];
-        for (let i = 0; i < tabNames.length; i++) {
-            tabs[i] = document.getElementById('tab' + tabNames[i]);
+        for (let i = 0; i < TAB_NAMES.length; i++) {
+            tabs[i] = document.getElementById('tab' + TAB_NAMES[i]);
             tabs[i].style.display = 'none';
         }
         let selectedTab = document.getElementById('tab' + tabName);
@@ -141,7 +171,7 @@ class Game{
 class Upgrade{
     constructor(state){
         this.state = state;
-        toDecimal(stateNames.upgrades[state.num - 1], state);
+        toDecimal(STATE_NAMES.upgrades[state.num - 1], state);
     }
 
     set increase(inc){
@@ -200,6 +230,13 @@ class Upgrade{
         return value.greaterThanOrEqualTo(this.currentCost);
     }
 
+    giveEffect(statement, statement2){
+        if (this.isBought) {
+            return statement;
+        }
+        return statement2;
+    }
+
     buy(obj, type){
         switch (type) {
             case 1: // Rebuyable upgrades; scaling costs, adding cps each time.
@@ -219,8 +256,8 @@ class Upgrade{
     }
 }
 
-const tabNames = ['upgrade-container', 'settings-container'];
-const stateNames = {
+const TAB_NAMES = ['upgrade-container', 'settings-container'];
+const STATE_NAMES = {
     main: [
         'coins',
         'cps'
@@ -231,87 +268,95 @@ const stateNames = {
             'multi'
         ],
         [
-            'inc',
-            'cost'
+            'cost',
+            'multi'
         ]
     ]
 };
-const baseNames = [
-    'cps'
-]
 let game = new Game();
-const interval = {
+const INTERVAL = {
     update: game.time,
     save: 15000
 };
-const version = {
+const VERSION = {
     type: 'P.O.C',
     no: '1.16'
 };
-const elements = [];
+const MAX_TIME = 50;
+const SECOND = 1000;
+const ELEMENTS = [];
 
-let init = function(){
+const INIT = function(){
     game.load();
-    document.title = "Jayman's Idle Project - " + version.type + ' ' + version.no;
-    elements[0] = document.getElementsByClassName('currency-info');
-    const currencyText = [' coins', ' coins/sec'];
-    const x = [1, 3];
+    document.title = "Jayman's Idle Project - " + VERSION.type + ' ' + VERSION.no;
+    ELEMENTS[0] = document.getElementsByClassName('currency-info');
+    const CURRENCY_TEXT = [' coins', ' coins/sec'];
+    const X = [1, 3];
     for (let i = 0; i < 2; i++) {
-        elements[0][x[i]].textContent = currencyText[i];
+        ELEMENTS[0][X[i]].textContent = CURRENCY_TEXT[i];
     }
-    elements[1] = document.getElementsByClassName('upgrade-buttons');
-    elements[2] = document.getElementsByClassName('upgrade-titles');
-    const upgradeTitles = ['Increase coin gain per second based on current coins.', 'Upgrade 2'];
-    elements[3] = document.getElementsByClassName('upgrade-cost-text');
-    for (let i = 0; i < elements[1].length; i++) {
-        elements[1][i].onclick = function(){ game.upgrades[i].buy(game, game.upgrades[i].type) };
-        elements[2][i].textContent = upgradeTitles[i];
-        elements[3][i].textContent = 'Cost: ';
+    ELEMENTS[1] = document.getElementsByClassName('upgrade-buttons');
+    ELEMENTS[2] = document.getElementsByClassName('upgrade-titles');
+    const UPGRADE_TITLES = ['Increase coin gain per second based on current coins.', 'Multiply coin gain per second by 2.'];
+    ELEMENTS[3] = document.getElementsByClassName('upgrade-cost-text');
+    for (let i = 0; i < ELEMENTS[1].length; i++) {
+        ELEMENTS[1][i].onclick = function(){ game.upgrades[i].buy(game, game.upgrades[i].type) };
+        ELEMENTS[2][i].textContent = UPGRADE_TITLES[i];
+        ELEMENTS[3][i].textContent = 'Cost: ';
     }
-    elements[4] = document.getElementsByClassName('upgrade-costs');
-    elements[5] = document.getElementsByClassName('tab-buttons');
-    elements[6] = document.getElementsByClassName('tab-titles');
-    const tabTitles = ['Upgrades', 'Settings'];
-    for (let i = 0; i < elements[5].length; i++) {
-        elements[5][i].onclick = function(){ game.showTab(tabNames[i]) };
-        elements[6][i].textContent = tabTitles[i];
+    ELEMENTS[4] = document.getElementsByClassName('upgrade-costs');
+    ELEMENTS[5] = document.getElementsByClassName('tab-buttons');
+    ELEMENTS[6] = document.getElementsByClassName('tab-titles');
+    const TAB_TITLES = ['Upgrades', 'Settings'];
+    for (let i = 0; i < ELEMENTS[5].length; i++) {
+        ELEMENTS[5][i].onclick = function(){ game.showTab(TAB_NAMES[i]) };
+        ELEMENTS[6][i].textContent = TAB_TITLES[i];
     }
-    elements[7] = document.getElementsByClassName('settings-buttons');
-    elements[8] = document.getElementsByClassName('settings-titles');
-    elements[7][0].onclick = function(){ game.save() };
-    elements[7][1].onclick = function(){ game.reset() };
-    const settingsNames = ['Save Game', 'Reset Game'];
-    for (let i = 0; i < elements[7].length; i++) {
-        elements[8][i].textContent = settingsNames[i];
+    ELEMENTS[7] = document.getElementsByClassName('settings-buttons');
+    ELEMENTS[8] = document.getElementsByClassName('settings-titles');
+    /*ELEMENTS[7][0].onclick = function(){ game.save() };
+    ELEMENTS[7][1].onclick = function(){ game.reset() };*/
+    const SETTINGS = ['game.save()', 'game.reset()'];
+    const SETTINGS_NAMES = ['Save Game', 'Reset Game'];
+    for (let i = 0; i < ELEMENTS[7].length; i++) {
+        ELEMENTS[7][i].onclick = function(){ eval(SETTINGS[i]) };
+        ELEMENTS[8][i].textContent = SETTINGS_NAMES[i];
     }
     let currentTime = Date.now();
     setInterval(function(){
-        const deltaTime = Date.now() - currentTime;
+        const DELTA_TIME = Date.now() - currentTime;
         currentTime = Date.now();
-        update.game(deltaTime);
+        update.game(DELTA_TIME);
         update.display();
-    }, interval.update);
+    }, INTERVAL.update);
     setInterval(function(){
         game.save();
-    }, interval.save);
+    }, INTERVAL.save);
 }
 
 let update = {
     game: function(ms){
-        game.time = game.addToValue(game.time, ms, 1, 3);
-        while (game.time > 50) {
-            game.time = game.subFromValue(game.time, 50, 2);
-            game.coins = game.addToValue(game.coins, game.coinsPerSec, 20, 1);
-            if (game.upgrades[0].isBought) game.coinsPerSec = game.multiplyValue(game.baseCoinsPerSec, game.upgrades[0].multiplier, 1);
+        game.time = game.addToValue(game.time, ms, 3);
+        while (game.time > MAX_TIME) {
+            game.time = game.subFromValue(game.time, MAX_TIME, 2);
+            game.coins = game.addToValue(game.coins, game.coinsPerSec, 1);
+            game.coinsPerSec = game.multiplyValue(game.baseCoinsPerSec, applyUpgradeEffects(), 1);
         }
     },
     display: function(){
-        elements[0][0].textContent = notation.scientific(game.coins);
-        elements[0][2].textContent = notation.scientific(game.coinsPerSec);
+        ELEMENTS[0][0].textContent = notation.scientific(game.coins);
+        ELEMENTS[0][2].textContent = notation.scientific(game.coinsPerSec);
         for (let i in game.upgrades) {
-            elements[4][i].textContent = notation.scientific(game.upgrades[i].currentCost);
+            ELEMENTS[4][i].textContent = notation.scientific(game.upgrades[i].currentCost);
         }
     }
+}
+
+let applyUpgradeEffects = function(){
+    let firstMulti = game.firstUpgrade.multiplier = game.firstUpgrade.giveEffect(game.addToValue(game.coins, 10, 4), game.baseMultiplier);
+    let secondEffect = game.secondUpgrade.giveEffect(game.secondUpgrade.multiplier, game.baseMultiplier); 
+    let totalEffect = game.multiplyValue(firstMulti, secondEffect, 2);
+    return totalEffect;
 }
 
 let toDecimal = function(keys, obj, useType){ // Thanks to etnpce for helping to refactor this function.
@@ -335,7 +380,7 @@ let notation = {
 }
 
 let start = window.onload = function(){
-    init();
+    INIT();
 }
 
 
